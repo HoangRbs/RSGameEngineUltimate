96a3547061e03bf31826039631ad6cdef373558e
HoangRbs <hoangrbs@gmail.com>
2021-04-29 18:43:39 +0700

    hoang rbs edit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import 'main.css';
 // testGame.BuildCanvas(500, 400);
 // testGame.Start();
 
-import GenAlgoUnicorn from './GenAlgoUnicorn';
+// import GenAlgoUnicorn from './GenAlgoUnicorn';
 
-let m_game = new GenAlgoUnicorn();
-m_game.BuildCanvas(500, 400);
-m_game.Start();
+// let m_game = new GenAlgoUnicorn();
+// m_game.BuildCanvas(500, 400);
+// m_game.Start();
