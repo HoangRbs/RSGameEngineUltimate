2d667feeb7c0a19a3111b261478153a6fc2ea5fc
HoangRbs <hoangrbs@gmail.com>
2021-06-01 16:24:39 +0700

    pratice coding interview

---
+++ src/AlgorithmDatastructure/Timer.js
@@ -0,0 +1,19 @@
+// how to use
+// let timer = new Timer();
+
+// code between
+
+// timer.end();
+
+export default class Timer {
+    constructor() {
+        this.start = performance.now();
+        this.end = this.start;
+    }
+
+    // to see time elapsed since the object is created, must call this function
+    endTimer() {
+        this.end = performance.now();
+        console.log(`Time elapsed: ${(this.end - this.start) / 1000} seconds`);
+    }
+}
---
+++ src/AlgorithmDatastructure/index.js
@@ -0,0 +1,9 @@
+import Timer from './Timer';
+
+function main() {
+    let timer = new Timer();
+    console.log("hello world");
+    timer.endTimer();
+}
+
+main();
